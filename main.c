741bf8b987c90f7ca52b5ee9d38d4a2d887c08c0
Clare Jarvis <jarvis@jarviscomputer.com>
2020-02-07 14:05:17 -0600

    -1 option working -v working any number of -t

--- main.c
+++ main.c
@@ -29,15 +29,13 @@
 static int mqtt_port=1883;
 static char mqtt_host[256];
 static char logDir[256]="logLocal";
-static int unitaryLogDir;
+static int unitaryLogFile;
 static char logFilePrefix[256];
 static char logFileSuffix[256];
 static int splitLogFilesByDay = 1;	
 static int flushAfterEachMessage = 1;
-#define MAX_TOPICS 16
-char *topics[MAX_TOPICS + 1];	// has a sentinel
-int topicsCount;
 
+int outputDebug=0;
 #if 0
 static char mqtt_topic[256];
 static struct mosquitto *mosq;
@@ -45,6 +43,56 @@ static struct mosquitto *mosq;
 
 extern char *optarg;
 extern int optind, opterr, optopt;
+
+char	*strsave(char *s )
+{
+char	*ret_val = 0;
+
+ret_val = malloc(strlen(s)+1);
+if ( 0 != ret_val) strcpy(ret_val,s);
+return ret_val;	
+}
+typedef struct topics {
+	struct topics *left,*right;
+	char	*topic;
+	}	TOPICS;
+
+TOPICS *topic_root = 0;
+
+void add_topic(char *s ) {
+	TOPICS *p,*q;
+	int	cond;
+	int	flag;
+
+	if ( 0 == topic_root ) {
+		topic_root = calloc(sizeof(TOPICS),1);
+		topic_root->topic = strsave(s);
+		return;
+	}
+	p = topic_root;
+	for ( ;0 != p; ) {
+		cond = strcmp(p->topic,s);
+		q = p;
+		if ( 0 == cond )	return;	// no reason to re-subscribe
+		if ( 0 < cond ) {
+			p = q->left;	flag = 1;
+		}
+		else {
+			p = q->right;	flag = -1;
+		}
+	}
+	/* if here then it is a new topic */
+	p = calloc(sizeof(TOPICS),1);
+	p->topic = strsave(s);
+	if ( 1 == flag )
+		q->left = p;
+	else
+		q->right = p;
+
+}
+
+
+
 
 uint64_t microtime() {
 	struct timeval time;
@@ -115,33 +163,12 @@ if ( 0 != rc )
 
 return	rc;
 }
-char	*strsave(char *s )
-{
-char	*ret_val = 0;
-
-ret_val = malloc(strlen(s)+1);
-if ( 0 != ret_val) strcpy(ret_val,s);
-return ret_val;	
-}
 	
 
-#define ALARM_SECONDS 10
+#define ALARM_SECONDS 600
 static int run = 1;
 
 
-void handle_signal(int signum) {
-	
-	if ( SIGALRM == signum ) {
-		/* keep alive modbus connection */
-
-	} else {
-		fprintf(stderr,"# %s signal received. Terminating.\n",strsignal(signum));
-		run = 0;
-		
-
-	}
-
-}
 
 void connect_callback(struct mosquitto *mosq, void *obj, int result) {
 	printf("# connect_callback, rc=%d\n", result);
@@ -149,9 +176,14 @@ void connect_callback(struct mosquitto *mosq, void *obj, int result) {
 
 void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message) {
 	char data[4];
-	bool match = 0;
-	char	_topic[256] = {};
-	int	i;
+	FILE	*out;
+	char	fname[256];
+	char	prefix[256] = {};
+	char	suffix[256] = {};
+	char	timestamp[32] = {};
+	char	path[256] = {};
+	struct tm *now;
+	time_t right_now;
 
 	/* cancel pending alarm */
 	alarm(0);
@@ -159,70 +191,73 @@ void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_
  	alarm(ALARM_SECONDS);
 
 
-	printf("got message '%.*s' for topic '%s'\n", message->payloadlen, (char*) message->payload, message->topic);
+	if ( outputDebug )
+		fprintf(stderr,"got message '%.*s' for topic '%s'\n", message->payloadlen, (char*) message->payload, message->topic);
 
 	/* local copy of our message */
 	strncpy(data, (char *) message->payload,3);
 	data[3]='\0';
 
-	
+	if ( -1 == time(&right_now) ) {
+		fprintf(stderr,"# error calling time() %s",strerror(errno));
+		exit(1);
+	}
+	now = localtime(&right_now);
+	if ( 0 == now ) {
+		fprintf(stderr,"# error calling localtime() %s",strerror(errno));
+		exit(1);
+	}
+	snprintf(timestamp,sizeof(timestamp),"%04d-%02d-%02d %02d:%02d:%02d,",
+		1900 + now->tm_year,1 + now->tm_mon, now->tm_mday,now->tm_hour,now->tm_min,now->tm_sec);
+		
 
-	for ( match = i = 0; 0 == match && topicsCount > i; i++ ) 
-		mosquitto_topic_matches_sub(topics[i], message->topic, &match);
-	strncpy(_topic,topics[i],sizeof(_topic));
+	if ( ' ' < logFilePrefix[0] )
+		strncpy(prefix,logFilePrefix,sizeof(prefix));
+	else {
+		snprintf(prefix,sizeof(prefix),"%04d%02d%02d",
+			1900 + now->tm_year,1 + now->tm_mon, now->tm_mday);
+	}
+	if ( ' ' < logFileSuffix[0] )
+		strncpy(suffix,logFileSuffix,sizeof(suffix));
+	if ( 0 == unitaryLogFile ) {
+		snprintf(path,sizeof(path),"%s/%s/",logDir,message->topic);
+		if ( _enable_logging_dir(path)) {
+			exit(1);
+		}
+		snprintf(fname,sizeof(fname),"%s/%s/%s%s",logDir,message->topic,prefix,suffix);
+	}
+	else snprintf(fname,sizeof(fname),"%s/%s%s",logDir,prefix,suffix);
+		
+	out = fopen(fname,"a");
+	if ( 0 == out )	{
+		fprintf(stderr,"# error calling fopen(%s) %s\n",fname,strerror(errno));
+		exit(1);
+	}
+	if ( -1 == fputs(timestamp,out)) {
+		fprintf(stderr,"# error calling fputs(%s) %s\n",fname,strerror(errno));
+		exit(1);
+	}
 
-	if (match) {
-		FILE	*out;
-		char	fname[256];
-		char	prefix[256] = {};
-		char	suffix[256] = {};
-		char	timestamp[32] = {};
-		struct tm *now;
-		time_t right_now;
-		fprintf(stderr,"got message for %s\n",message->topic);
-		if ( -1 == time(&right_now) ) {
-			fprintf(stderr,"# error calling time() %s",strerror(errno));
-			exit(1);
-		}
-		now = localtime(&right_now);
-		if ( 0 == now ) {
-			fprintf(stderr,"# error calling localtime() %s",strerror(errno));
-			exit(1);
-		}
-		snprintf(timestamp,sizeof(timestamp),"%04d-%02d-%02d %02d:%02d:%02d,",
-			1900 + now->tm_year,1 + now->tm_mon, now->tm_mday,now->tm_hour,now->tm_min,now->tm_sec);
-			
-
-		if ( ' ' < logFilePrefix[0] )
-			strncpy(prefix,logFilePrefix,sizeof(prefix));
-		else {
-			snprintf(prefix,sizeof(prefix),"%04d%02d%02d",
-				1900 + now->tm_year,1 + now->tm_mon, now->tm_mday);
-		}
-		if ( ' ' < logFileSuffix[0] )
-			strncpy(suffix,logFileSuffix,sizeof(suffix));
-		snprintf(fname,sizeof(fname),"%s/%s/%s%s",logDir,_topic,prefix,suffix);
-		out = fopen(fname,"a");
-		if ( 0 == out )	{
-			fprintf(stderr,"# error calling fopen(%s) %s\n",fname,strerror(errno));
-			exit(1);
-		}
-		if ( -1 == fputs(timestamp,out)) {
-			fprintf(stderr,"# error calling fputs(%s) %s\n",fname,strerror(errno));
-			exit(1);
-		}
-
-		if ( 1 != fwrite(message->payload,message->payloadlen,1,out )) {
-			fprintf(stderr,"# error calling fwrite(%s) %s\n",fname,strerror(errno));
-			exit(1);
-		}
-		if ( 0 != fclose(out)){
-			fprintf(stderr,"# error calling fclose(%s) %s\n",fname,strerror(errno));
-			exit(1);
-		}
+	if ( 1 != fwrite(message->payload,message->payloadlen,1,out )) {
+		fprintf(stderr,"# error calling fwrite(%s) %s\n",fname,strerror(errno));
+		exit(1);
+	}
+	if ( -1 == fputc('\n',out)) {
+		fprintf(stderr,"# error calling fputc(%s) %s\n",fname,strerror(errno));
+		exit(1);
+	}
+	if ( 0 != fclose(out)){
+		fprintf(stderr,"# error calling fclose(%s) %s\n",fname,strerror(errno));
+		exit(1);
 	}
 }
-
+void topics_mosquitto_subscribe(TOPICS *p, struct mosquitto *mosq)
+{
+if ( 0 == p )	return;
+topics_mosquitto_subscribe(p->left,mosq);
+mosquitto_subscribe(mosq, NULL, p->topic, 0);
+topics_mosquitto_subscribe(p->right,mosq);
+}
 static int startup_mosquitto(void) {
 	char clientid[24];
 	struct mosquitto *mosq;
@@ -231,9 +266,9 @@ static int startup_mosquitto(void) {
 	fprintf(stderr,"# mqtt-modbus start-up\n");
 
 	fprintf(stderr,"# installing signal handlers\n");
-	signal(SIGINT, handle_signal);
-	signal(SIGTERM, handle_signal);
-	signal(SIGALRM, handle_signal);
+	signal(SIGINT, signal_handler);
+	signal(SIGTERM, signal_handler);
+	signal(SIGALRM, signal_handler);
 
 	fprintf(stderr,"# initializing mosquitto MQTT library\n");
 	mosquitto_lib_init();
@@ -243,15 +278,13 @@ static int startup_mosquitto(void) {
 	mosq = mosquitto_new(clientid, true, 0);
 
 	if (mosq) {
-		int i;
 		mosquitto_connect_callback_set(mosq, connect_callback);
 		mosquitto_message_callback_set(mosq, message_callback);
 
 		fprintf(stderr,"# connecting to MQTT server %s:%d\n",mqtt_host,mqtt_port);
 		rc = mosquitto_connect(mosq, mqtt_host, mqtt_port, 60);
 
-		for ( i = 0; topicsCount > i; i++ )
-			mosquitto_subscribe(mosq, NULL, topics[i], 0);
+		topics_mosquitto_subscribe(topic_root,mosq);
 
 		while (run) {
 			rc = mosquitto_loop(mosq, -1, 1);
@@ -271,22 +304,31 @@ static int startup_mosquitto(void) {
 	return rc;
 }
 
+int topics_enable_logging_dir(TOPICS *p )
+{
+int	rc;
+if ( 0 == p )	return	0;
+rc = topics_enable_logging_dir(p->left);
+if ( 0 == rc ) rc =	_enable_logging_dir(p->topic);
+if ( 0 == rc ) topics_enable_logging_dir(p->right);
+return	rc;
+}
 
 int main(int argc, char **argv) {
-	int i,n;
+	int n;
 	int rc;
+	char	cwd[256] = {};
+	(void) getcwd(cwd,sizeof(cwd));
 
 	/* command line arguments */
-	while ((n = getopt (argc, argv, "s:p:l:D:dfFt:h")) != -1) {
+	while ((n = getopt (argc, argv, "1vs:p:l:D:dfFt:h")) != -1) {
 		switch (n) {
 			case 's':	
 				strncpy(logFileSuffix,optarg,sizeof(logFileSuffix));
 				fprintf(stderr,"# logFileSuffix=%s \n",logFileSuffix);
 				break;
 			case 't':
-				if ( topicsCount < MAX_TOPICS )
-					topics[topicsCount] = strsave(optarg);
-				topicsCount++;	// always count so we can detect too many -t
+				add_topic(optarg);
 				break;
 			case 'p':
 				mqtt_port = atoi(optarg);
@@ -295,8 +337,8 @@ int main(int argc, char **argv) {
 				strncpy(logDir,optarg,sizeof(logDir));
 				break;
 			case '1':
-				unitaryLogDir=1;
-				fprintf(stderr,"# unitaryLogDir\tlogging to one directory\n");
+				unitaryLogFile=1;
+				fprintf(stderr,"# unitaryLogFile\tlogging to one directory\n");
 				break;
 			case 'D':	
 				strncpy(logFilePrefix,optarg,sizeof(logFilePrefix));
@@ -315,15 +357,20 @@ int main(int argc, char **argv) {
 				flushAfterEachMessage = 0;
 				fprintf(stderr,"# flushAfterEachMessage=%s \n","DISABLED");
 				break;
+			case 'v':
+				outputDebug=1;
+				fprintf(stderr,"# verbose (debugging) output to stderr enabled\n");
+				break;
 			case 'h':
 				fprintf(stdout,"# -s\t\tend each log filename with suffix\n");
-				fprintf(stdout,"# -t\t\tmqtt topic must be used once and 1-%d times\n",MAX_TOPICS);
+				fprintf(stdout,"# -t\t\tmqtt topic must be used at least once\n");
 				fprintf(stdout,"# -p\t\tmqtt port\t\tOPTIONAL\n");
 				fprintf(stdout,"# -l\t\tlogging derectory, default=logLocal\n");
-				fprintf(stdout,"# -1\t\tunitaryLogDir\n");
+				fprintf(stdout,"# -1\t\tunitaryLogFile\n");
 				fprintf(stdout,"# -D\t\tstart each log filename with prefix rather than YYMMDD\n");
 				fprintf(stdout,"# -d\t\tstart each log file with YYMMDD, start new file each day.  (default)\n");
 				fprintf(stdout,"# -f\t\tflushAfterEachMessage recieved to log file\n");
+				fprintf(stdout,"# -v\t\tOutput verbose / debugging to stderr\n");
 				fprintf(stdout,"#\n");
 				fprintf(stdout,"# -h\t\tThis help message then exit\n");
 				fprintf(stdout,"#\n");
@@ -336,18 +383,11 @@ int main(int argc, char **argv) {
 	}
 	else
 		strncpy(mqtt_host,argv[optind],sizeof(mqtt_host));	
-	if ( 0 == topicsCount ) {
-		fprintf(stderr,"# There must be at least one -t and no more than %d -t\n",MAX_TOPICS);
+	if ( 0 == topic_root ) {
+		fprintf(stderr,"# There must be at least one -t\n");
                exit(EXIT_FAILURE);
 	}
 
-	if ( MAX_TOPICS < topicsCount ) {
-		fprintf(stderr,"# %d MAX_TOPICS < %d topicsCount\n",MAX_TOPICS,topicsCount);
-               exit(EXIT_FAILURE);
-	}
-
-
-		
 
 	/* install signal handler */
 	signal(SIGALRM, signal_handler); /* timeout */
@@ -358,9 +398,10 @@ int main(int argc, char **argv) {
 		return	1;
 	}
 	chdir(logDir);
-	for ( i = 0; topicsCount > i; i++ )
-		if (  _enable_logging_dir(topics[i]))
-			return	1;
+	if ( topics_enable_logging_dir(topic_root))
+		return	1;
+
+	chdir(cwd);
 	rc = startup_mosquitto();
 	
 
